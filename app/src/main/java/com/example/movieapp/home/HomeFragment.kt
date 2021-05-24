e7086087bfeea266ff3235142dccb139dd3b36b4
MyCapstone <alifaliki456@gmail.com>
2021-05-24 20:01:07 +0700

    Removing unused file or resource

--- app/src/main/java/com/example/movieapp/home/HomeFragment.kt
+++ app/src/main/java/com/example/movieapp/home/HomeFragment.kt
@@ -1,17 +1,15 @@
 package com.example.movieapp.home
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityOptionsCompat
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.movieapp.R
 import com.example.core.data.Resource
 import com.example.core.ui.PopularMovieAdapter
+import com.example.movieapp.R
 import com.example.movieapp.databinding.FragmentHomeBinding
 import com.example.movieapp.detail.DetailActivity
 import org.koin.android.viewmodel.ext.android.viewModel
